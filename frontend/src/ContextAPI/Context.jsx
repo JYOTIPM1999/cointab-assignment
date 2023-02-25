9e32745cd8fc49554c12c5af977e5d8bcb1ed987
JYOTIPM1999 <jyotipm1999@gmail.com>
2023-02-25 21:58:39 +0530

    User loading fetch complete

--- frontend/src/ContextAPI/Context.jsx
+++ frontend/src/ContextAPI/Context.jsx
@@ -3,9 +3,24 @@ import React, { createContext, useState } from "react";
 export const userContext = createContext();
 
 const ContextProvider = ({ children }) => {
+  const [count, setCount] = useState(0);
   const [allUsers, setAllUsers] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [deleteUsers, setDeleteUsers] = useState(false);
+
   return (
-    <userContext.Provider value={{ allUsers, setAllUsers }}>
+    <userContext.Provider
+      value={{
+        allUsers,
+        setAllUsers,
+        loading,
+        setLoading,
+        deleteUsers,
+        setDeleteUsers,
+        count,
+        setCount,
+      }}
+    >
       {children}
     </userContext.Provider>
   );
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,4 +1,14 @@
-import { Box, Button, Flex, Image, useToast } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  CircularProgress,
+  Flex,
+  Image,
+  Text,
+  useToast,
+} from "@chakra-ui/react";
+
+import { Grid } from "react-loader-spinner";
 import React, { useContext, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
@@ -6,10 +16,16 @@ import DetailsPage from "./DetailsPage";
 import { userContext } from "../ContextAPI/Context";
 
 const HomePage = () => {
-  const [loading, setLoading] = useState(false);
-  const [deleteUsers, setDeleteUsers] = useState(false);
   const navigate = useNavigate();
-  const { setAllUsers } = useContext(userContext);
+  const {
+    setAllUsers,
+    loading,
+    setLoading,
+    deleteUsers,
+    setDeleteUsers,
+    count,
+    setCount,
+  } = useContext(userContext);
 
   const toast = useToast();
   const Toasting = (title, desc, pos, status) => {
@@ -35,9 +51,10 @@ const HomePage = () => {
 
     setLoading(true);
     axios.post("http://localhost:8080/users/savedata").then((res) => {
-      console.log(res.data.usr);
-      setAllUsers(res.data.usr);
+      // console.log(res.data.usr);
+      // setAllUsers(res.data.usr);
       setLoading(false);
+      setCount(res.data.count);
       Toasting(
         "Successfully Fetched",
         `count is ${res.data.count}`,
@@ -51,6 +68,7 @@ const HomePage = () => {
     setDeleteUsers(true);
     axios.delete("http://localhost:8080/users/deletedata").then((res) => {
       setAllUsers([]);
+      setCount(0);
       setDeleteUsers(false);
       Toasting("Users deleted", "All Users data deleted", "buttom", "error");
       console.log(res.data);
@@ -59,7 +77,7 @@ const HomePage = () => {
   const handleDetails = () => {
     navigate("/details");
   };
-
+  console.log(count);
   return (
     <Box m={"auto"} textAlign={"center"}>
       <Image src="/cointab.png" m={"auto"} />
@@ -67,11 +85,52 @@ const HomePage = () => {
       <br />
       <Flex justifyContent="space-around">
         <Button onClick={handleFetch}>
-          {loading ? "Loading..." : "Fetch User"}
+          {loading ? (
+            <CircularProgress size={"30px"} isIndeterminate color="green.300" />
+          ) : (
+            "Fetch User"
+          )}
         </Button>
-        <Button onClick={handleDelete}>Delete Users</Button>
-        <Button onClick={handleDetails}>Details User Page</Button>
+        <Box>
+          {count === 0 && <Text color={"red"}>Please fetch first</Text>}
+          <Button isDisabled={count === 0} onClick={handleDelete}>
+            Delete Users
+          </Button>
+        </Box>
+        <Box>
+          {count === 0 && <Text color={"red"}>Please fetch first</Text>}
+          <Button isDisabled={count === 0} onClick={handleDetails}>
+            Details User Page
+          </Button>
+        </Box>
       </Flex>
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      {loading ? (
+        <Text fontWeight={"bold"} color={"red"} textAlign="center">
+          {/* <CircularProgress isIndeterminate color="green.500" /> */}
+          {/* Fething takes time, wait for it to complete... */}
+          <br />
+          <Grid
+            height="80"
+            width="80"
+            color="#4fa94d"
+            ariaLabel="grid-loading"
+            radius="12.5"
+            wrapperStyle={{}}
+            wrapperClass=""
+            visible={true}
+          />
+          <br />
+        </Text>
+      ) : (
+        <Text fontWeight={"bold"} color={"green"}>
+          User already fetched you can fetch again...
+        </Text>
+      )}
     </Box>
   );
 };
